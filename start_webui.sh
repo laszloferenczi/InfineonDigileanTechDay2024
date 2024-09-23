5c45d8f072f801c67337d532e3e94a409c5dfe85
laszloferenczi <laci@OTI-PC-01>
2024-09-23 15:30:59 +0200

    docker command modified

--- start_webui.sh
+++ start_webui.sh
@@ -1,1 +1,4 @@
+#/bin/bash
+sudo docker stop open-webui
+sudo docker container rm open-webui
 sudo docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=$1 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
